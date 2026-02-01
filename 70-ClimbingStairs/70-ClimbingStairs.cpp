8ac669e1e65e6c10bb3fbec90964902edaa8212e
utkarsh-112 <79863085+utkarsh-112@users.noreply.github.com>
2026-02-01 21:17:59 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,16 @@
+// Last updated: 01/02/2026, 21:17:59
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        int prev = 2, prev2 = 1, res;
+        for (int i = 3; i <= n; i++)
+        {
+            res = prev + prev2;
+            prev2 = prev;
+            prev = res;
+        }
+        return res;
+    }
+};
