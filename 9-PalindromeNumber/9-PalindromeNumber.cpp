6dcf66016370b770f7ee0a9bb8f604a07451a210
utkarsh-112 <79863085+utkarsh-112@users.noreply.github.com>
2026-02-01 21:18:50 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 01/02/2026, 21:18:49
+   class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
