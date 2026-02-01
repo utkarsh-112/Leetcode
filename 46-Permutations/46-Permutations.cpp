53eca6a45b574d241a6475af66df2780ccc986b0
utkarsh-112 <79863085+utkarsh-112@users.noreply.github.com>
2026-02-01 21:18:15 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.cpp
@@ -0,0 +1,20 @@
+// Last updated: 01/02/2026, 21:18:14
+class Solution {
+public:
+    void solve(int ind, vector<int>& nums,vector<vector<int>>& ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            solve(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
