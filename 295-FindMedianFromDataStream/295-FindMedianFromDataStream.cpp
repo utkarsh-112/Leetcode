8b0336a799c0351187c52eccd39ba7b7cff89cff
utkarsh-112 <79863085+utkarsh-112@users.noreply.github.com>
2026-02-01 21:16:57 +0530

    Create 295-FindMedianFromDataStream

---
+++ 295-FindMedianFromDataStream/295-FindMedianFromDataStream.cpp
@@ -0,0 +1,31 @@
+// Last updated: 01/02/2026, 21:16:57
+class MedianFinder {
+public:
+    priority_queue<double> maxHeap;
+    priority_queue<double,vector<double>,greater<double>> minHeap;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top()+minHeap.top())/2.0;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
