166c383f0cf5faab69439be847da424b64cf2885
utkarsh-112 <79863085+utkarsh-112@users.noreply.github.com>
2026-02-01 21:17:49 +0530

    Create 89-GrayCode

---
+++ 89-GrayCode/89-GrayCode.cpp
@@ -0,0 +1,10 @@
+// Last updated: 01/02/2026, 21:17:48
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans(1 << n);
+        for (int i = 0; i < (1 << n); i++)
+            ans[i] = i ^ (i >> 1);
+        return ans;
+    }
+};
